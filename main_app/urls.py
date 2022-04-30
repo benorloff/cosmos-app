0238eb324e56359dde4946b50fab5adeb9f2cd72
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-04-30 16:31:09 -0700

    comment out unused paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,8 +3,8 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('events/', views.EventList.as_view(), name='events_list'),
-    path('events/create/', views.EventCreate.as_view(), name='events_create'),
-    path('events/<int:pk>/', views.EventDetail.as_view(), name='events_detail'),
-    path('events/<int:pk>/update/', views.EventUpdate.as_view(), name='events_update'),
+    # path('events/', views.EventList.as_view(), name='events_list'),
+    # path('events/create/', views.EventCreate.as_view(), name='events_create'),
+    # path('events/<int:pk>/', views.EventDetail.as_view(), name='events_detail'),
+    # path('events/<int:pk>/update/', views.EventUpdate.as_view(), name='events_update'),
 ]
