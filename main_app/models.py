05b6fa598d102498e9d6d360ad78dcbf86b6ba81
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-04-29 17:09:04 -0700

    merge

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+<<<<<<< HEAD
 # Create your models here.
 
 class Event(models.Model):
@@ -51,3 +52,6 @@ class ViewingParty(models.Model):
 
     def __str__(self):
         return f"Viewing party is {self.name}."
+=======
+# Create your models here.
+>>>>>>> 8416e85 (django dotenv)
