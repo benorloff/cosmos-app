fb8b266cafa82b72b856c277886291560529b47d
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-04-30 16:42:40 -0700

    add imports to views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+import uuid
+import boto3
+
+from .models import Event, ViewingParty, Profile, Photo
 
 # Create your views here.
 
