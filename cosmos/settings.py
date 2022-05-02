b553cb14cd16d087bb79e3ee0797d532d3ee64f8
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-05-02 09:40:00 -0700

    add login redirect url

--- cosmos/settings.py
+++ cosmos/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = '/events/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
