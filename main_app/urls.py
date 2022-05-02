4e46da98ea96326fe8825968ad994639e947a5fc
Peter Luong <peterluong@Peters-MBP.socal.rr.com>
2022-05-02 09:57:46 -0700

    added /accounts/signup route

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accounts/signup/', views.signup, name='signup'),
     path('events/', views.EventList.as_view(), name='events_list'),
     # path('events/create/', views.EventCreate.as_view(), name='events_create'),
     # path('events/<int:pk>/', views.EventDetail.as_view(), name='events_detail'),
