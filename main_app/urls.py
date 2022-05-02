55e98bc7a52140232dee99cd0bdd30b83a342002
Peter Luong <peterluong@Peters-MBP.socal.rr.com>
2022-05-02 11:28:03 -0700

    added path for add_photo

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('events/', views.EventList.as_view(), name='events_list'),
     path('profile/', views.profile, name='profile')
+    path('profile/<int:pk>/add_photo', views.add_photo, name='add_photo'),
     # path('events/create/', views.EventCreate.as_view(), name='events_create'),
     # path('events/<int:pk>/', views.EventDetail.as_view(), name='events_detail'),
     # path('events/<int:pk>/update/', views.EventUpdate.as_view(), name='events_update'),
