2d20ba12b607eaae4e38ea7a1bfc161ff16a8438
Peter Luong <peterluong@Peters-MBP.socal.rr.com>
2022-05-02 10:40:32 -0700

    added route from profiles

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('events/', views.EventList.as_view(), name='events_list'),
+    path('profile/', views.profile, name='profile')
     # path('events/create/', views.EventCreate.as_view(), name='events_create'),
     # path('events/<int:pk>/', views.EventDetail.as_view(), name='events_detail'),
     # path('events/<int:pk>/update/', views.EventUpdate.as_view(), name='events_update'),
