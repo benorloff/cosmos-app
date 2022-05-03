d91baac9efb7ab392e11e7ce11c1d76eddc5f12d
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-05-02 21:01:19 -0700

    django allauth templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('accounts/signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
     path('profile/<int:profile_id>/add_photo', views.add_photo, name='add_photo'),
     path('events/', views.EventList.as_view(), name='events_list'),
