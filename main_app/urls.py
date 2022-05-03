f1d5cc5d0e2b8874d03c6752e839ca5056e86a5c
Peter Luong <peterluong@Peters-MBP.socal.rr.com>
2022-05-03 09:57:22 -0700

    adjusted profile_id/addphoto to user_id/addphoto

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
-    path('profile/<int:profile_id>/add_photo', views.add_photo, name='add_photo'),
+    path('profile/<int:user_id>/add_photo', views.add_photo, name='add_photo'),
     path('events/', views.EventList.as_view(), name='events_list'),
     path('events/create/', views.EventCreate.as_view(), name='events_create'),
     path('events/<int:pk>/', views.EventDetail.as_view(), name='events_detail'),
