9813d85049de739e5916ca86c5be7a57ab4507ea
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-05-03 08:40:50 -0700

    inline profile for user admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 from .models import Event, Profile, Photo, ViewingParty
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Event)
