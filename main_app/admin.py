ae8f5fdcae7aff4138e4332ccdfebf32b7d45aa4
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-05-04 08:23:33 -0700

    remove Task references from admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,10 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from .models import Event, Profile, Photo, ViewingParty
 
-
 from import_export.admin import ImportExportMixin
-from .models import Task
-
 
 class ProfileInline(admin.StackedInline):
     model = Profile
@@ -16,14 +13,8 @@ class ProfileInline(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = (ProfileInline,)
 
-
-class TaskAdmin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ['name', 'description', 'due_date', 'is_complete']
-
-
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
-admin.site.register(Task, TaskAdmin)
 admin.site.register(Event)
 admin.site.register(ViewingParty)
 admin.site.register(Photo)
