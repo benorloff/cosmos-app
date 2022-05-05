fcb1e72617f4071e1f61d80f2dfbee64df1e6d59
virtual-luna <maya.ellington@berkeley.edu>
2022-05-05 16:03:56 -0700

    deleted datetime code and moving js into main app folder

--- cosmos/static/js/main.js
+++
@@ -1,8 +0,0 @@
-const filterEl = document.querySelector('.dropdown-trigger');
-const tooltipEls = document.querySelectorAll('.tooltipped');
-M.Dropdown.init(filterEl, {
-    alignment: 'bottom',
-    coverTrigger: false,
-    constrainWidth: false
-});
-M.Tooltip.init(tooltipEls);
--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -3,5 +3,13 @@ $(document).ready(function(){
     $('.dropdown-trigger').dropdown();
  });
 
-
+ const filterEl = document.querySelector('.dropdown-trigger');
+ const tooltipEls = document.querySelectorAll('.tooltipped');
+ M.Dropdown.init(filterEl, {
+     alignment: 'bottom',
+     coverTrigger: false,
+     constrainWidth: false
+ });
+ M.Tooltip.init(tooltipEls);
+ 
         
