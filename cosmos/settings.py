11adacaf7e46dfd0d43ea5d5aed8abdc7f512f22
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-06-14 09:23:09 -0700

    remove auth email verification

--- cosmos/settings.py
+++ cosmos/settings.py
@@ -168,6 +168,8 @@ LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
